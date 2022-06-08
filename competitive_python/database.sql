8479f0e23fd822b2e850dd9369f6a9ded8d843b0
Joao Victor Cardoso dos Santos <57873270+jvcss@users.noreply.github.com>
2022-06-08 03:59:55 -0300

    add livro definition fix
    fix livro referencia para area do conhecimento

--- competitive_python/database.sql
+++ competitive_python/database.sql
@@ -1,64 +1,80 @@
 -- **********************************************
+--- DCL – Data Control Language
+--- DELETE EVERY TABLE
+
+DO $$ DECLARE
+    rec RECORD;
+BEGIN
+    FOR rec IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(rec.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
+--- DELETE enumerador
+DROP TYPE users;
+
+
+
+-- **********************************************
 -- CREATE       -- DDL – Data Definition Language
 /*
--- CREATE       -- DDL – Data Definition Language
-CREATE TABLE "dados"(
-  "dados_id_user" SERIAL PRIMARY KEY,
-  "dado_nome" varchar,
-  "dado_telefone" varchar,
-  "dado_endereco" varchar
+CREATE TYPE users AS ENUM ('funcionarios', 'alunos', 'professores');
+CREATE TABLE dados(
+  dados_id_user SERIAL PRIMARY KEY,
+  dado_nome varchar,
+  dado_telefone varchar,
+  dado_endereco varchar
 );
-CREATE TABLE "areas_conhecimento" (
-  "codigo_area_conhecimento" SERIAL PRIMARY KEY,
-  "descricao_area" varchar
+CREATE TABLE areas_conhecimento (
+  area_conhecimento_id SERIAL PRIMARY KEY,
+  descricao_area varchar
 );
-
-
-/*CREATE TABLE "funcionarios" (
-  "codigo_funcionario" SERIAL PRIMARY KEY,
-  "dados_id"  int REFERENCES dados (dados_id_user) ON DELETE CASCADE -- link para dados dados_id_user
+CREATE TABLE funcionarios (
+  funcionario_id SERIAL PRIMARY KEY,
+  dados_id_funcionarios  int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE
+  -- link para dados dados_id_user
 );
-CREATE TABLE "alunos" (
-  "codigo_aluno" SERIAL PRIMARY KEY,
-  "dados_id" int REFERENCES dados (dados_id_user) ON DELETE CASCADE
+CREATE TABLE alunos (
+  aluno_id SERIAL PRIMARY KEY,
+  dados_id_alunos int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE TABLE "professores" (
-  "codigo_professor" SERIAL PRIMARY KEY,
-  "dados_id" int REFERENCES dados (dados_id_user) ON DELETE CASCADE
+CREATE TABLE professores (
+  professor_id SERIAL PRIMARY KEY,
+  dados_id_professores int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE
 );
-CREATE TABLE "usuario" (
-  "usuario_id" SERIAL PRIMARY KEY,
-  "tipo" varchar,--link um para vários, tipo pode ser id_funcionario/_aluno/_professor
-  "inicio_locacao_dia" date DEFAULT (now()),
-  "fim_locacao_dia" date DEFAULT (now()+ INTERVAL '7 DAY')
-);ALTER TABLE `usuario` ADD FOREIGN KEY (`tipo`, `tipo`, `tipo`) REFERENCES `users` (`aluno`, `professor`, `funcionario`);
-
-CREATE TABLE "exemplares" (
-  "exemplar_id" SERIAL PRIMARY KEY,
-  "locador_id_usuario" INT REFERENCES usuario (usuario_id), -- link para usuario id
-  "exemplar_id_livro" varchar,
-  -- cada exemplar tem um id, para locar deve-se verificar o filtro de quantidades já locadas desse exemplar_id
-  "unidades" INTEGER
-);*/
-CREATE TABLE "editora" (
-  "editora_id" SERIAL PRIMARY KEY,
+CREATE TABLE usuario (
+  usuario_id SERIAL PRIMARY KEY,
+  -- table usuario_id onde tipo == users
+  tipo_usuario users,
+  inicio_locacao_dia date DEFAULT (now()),
+  fim_locacao_dia date DEFAULT (now()+ INTERVAL '7 DAY')
+);
+CREATE TABLE editora (
+  editora_id SERIAL PRIMARY KEY,
+  dados_id_editora int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE
   --"titulos" varchar, -- editora tem uma tabela nova de livros, ou seja, só um filtro e não uma input
-  "dados_id" int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE TABLE autor (
   autor_id  serial PRIMARY KEY,
-  dados_id  int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE 
+  dados_id_autor int REFERENCES dados (dados_id_user) ON UPDATE CASCADE ON DELETE CASCADE 
   -- CRIANDO UM dados_id associamos ao dados ID das tabela
   -- isso permite consulta concatenada
 );
 CREATE TABLE livro (
   livro_id serial PRIMARY KEY, -- implicit primary key constraint
   livro_nome VARCHAR NOT NULL,
-  autor_id int REFERENCES autor (autor_id) ON UPDATE CASCADE
+  autor_id int REFERENCES autor (autor_id) ON UPDATE CASCADE,
   -- existe um autor principal, mas também é um filtro na tabela de relação autores por livros
   editora_id int REFERENCES editora (editora_id) ON UPDATE CASCADE
+  livro_id_areas_conhecimento int REFERENCES areas_conhecimento (area_conhecimento_id) ON UPDATE CASCADE
   -- valor_monetario numeric NOT NULL DEFAULT 0
   -- conteudo BLOB NOT NULL -- podemos salvar livros inteiros como imagens em um retrato e ir cortando blocos para fazer paginas 
+);
+CREATE TABLE exemplares (
+  exemplar_id SERIAL PRIMARY KEY,
+  locador_id_usuario INT REFERENCES usuario (usuario_id), -- link para usuario id
+  exemplar_id_livro INT REFERENCES livro (livro_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  -- cada exemplar tem um id, para locar deve-se verificar o filtro de quantidades já locadas desse exemplar_id
+  unidades int
 );
 CREATE TABLE autor_por_livro (
   livro_id int REFERENCES livro (livro_id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -69,19 +85,23 @@ CREATE TABLE autor_por_livro (
   CONSTRAINT autor_livro_pkey PRIMARY KEY (livro_id,autor_id)-- explicit pk
 );
 
---ALTER TABLE `editora` ADD FOREIGN KEY (`dados_id`) REFERENCES `dados` (`dados_id_user`);
---ALTER TABLE `autor` ADD FOREIGN KEY (`dados_id`) REFERENCES `dados` (`dados_id_user`);
-
 */
 
 -- **********************************************
 -- DQL – Data Query Language
 
 --- INSERT
-
---INSERT INTO autor (autor_nome) VALUES ('victor'),('andrezza')
---INSERT INTO livro (livro_nome,autor_id) VALUES ('O Idiota',1), ('Pequeno Principe',1), ('O Processo',2)
---INSERT INTO autor_por_livro (livro_id, autor_id) VALUES (1,1)(2,1)
+--INSERT INTO dados(dado_nome, dado_telefone, dado_endereco) VALUES('joao', '6298', 'rua asa')
+/* see DML example (1)
+--- TABLE TO ALL ADDRESSES FOR EVERONE: funcionarios, alunos, professores,editoras,autores
+--- no metter who is, if it's here we can seach by it's ID
+ _______________________________________________
+|ID | DADO_NOME | DADO_TELEFONE | DADO_ENDEREÇO|
+|---|-----------|---------------|--------------|
+| 1 | joao      | 6298          | rua asa      |
+|---|-----------|---------------|--------------|
+|___|___________|_______________|______________|
+*/
 
 
 -- **********************************************
@@ -89,63 +109,26 @@ CREATE TABLE autor_por_livro (
 
 --- SHOW       
 
+--SELECT * FROM dados
 --SELECT * FROM autor
---SELECT * FROM livro
---SELECT * FROM autor_por_livro
---SELECT livro_id FROM autor_por_livro WHERE autor_id = 1
 
-/*
+
+-- Recursivity seach concatenade to grab joao's dados(table)
+-- that's the gimmick valid for the entire relationship between datas since now
+/* see DQL example (1)
 SELECT
-    * -- seleciona o mesmo livro para vários autores
+    * -- pega todo conteudo de dados
 FROM
-    autor
+    dados
 WHERE
-    autor_id IN -- this can also be "NOT IN", "EXISTS, an operator like "=", "<", and others.
+    dados_id_user IN -- this can also be "NOT IN", "EXISTS, an operator like "=", "<", and others.
     (
     SELECT
-        autor_id
+        dados_id_autor
     FROM
-        autor_por_livro
+        autor
     WHERE
-        livro_id = 2
+        autor_id = 1
     )
 */
 
-/*SELECT
-    * -- seleciona o mesmo autor para vários livros
-FROM
-    livro
-WHERE
-    livro_id IN -- this can also be "NOT IN", "EXISTS, an operator like "=", "<", and others.
-    (
-    SELECT
-        livro_id
-    FROM
-        autor_por_livro
-    WHERE
-        autor_id = 1
-    )*/
-
-
-
--- **********************************************
--- DCL – Data Control Language
-
---- CLEAR 
-
--- TRUNCATE TABLE autor_por_livro
-
---- DELETE
-
-/*
-DO $$ DECLARE
-    rec RECORD;
-BEGIN
-    -- if the schema you operate on is not "current", you will want to
-    -- replace current_schema() in query with 'schematodeletetablesfrom'
-    -- *and* update the generate 'DROP...' accordingly.
-    FOR rec IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(rec.tablename) || ' CASCADE';
-    END LOOP;
-END $$;
-*/
