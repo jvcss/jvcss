448b9a1f775b9d5745ae3434c4a14f6831064435
jvcss <57873270+jvcss@users.noreply.github.com>
2026-02-26 14:49:21 +0000

    Deploying to gh-pages from @ jvcss/jvcss@a44509aab45ca69b557413cbb12dc8e32694fc27 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1796284721" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "903593468" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
